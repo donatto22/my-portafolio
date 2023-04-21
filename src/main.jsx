49866616dc4295915de4e5f93351af9a1089ab19
donatto22 <ottanod22@gmail.com>
2023-04-20 21:05:44 -0500

    Nuevo cursor

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,21 @@ import Contact from './pages/contact/Contact'
 import Projects from './pages/projects/Projects'
 import OutletManager from './outlet'
 
+import AnimatedCursor from "react-animated-cursor"
+
 createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
+        <AnimatedCursor color="255,120,155"
+            innerSize={8}
+            outerSize={45}
+            innerScale={1}
+            outerScale={1.5}
+            outerAlpha={0}
+            outerStyle={{
+                border: '2px solid var(--discord-blue2)'
+            }}
+        />
+
         <Provider store={store}>
             <Routes>
                 <Route path='/' element={<OutletManager />}>
